938182bcc87fbe3253f9a133e21fee35ae6e8b88
Drew Dodson <drewdodson@cox.net>
2020-01-23 09:32:18 -0800

    Completed 'Assignment02.py'

---
+++ Assignment02.py
@@ -0,0 +1,8 @@
+# Assignment 02
+# CSE-40028, 145020, Winter 2020
+# Robert Dodson
+
+PI = 3.1415
+diameter_of_circle = 3
+circumfrence_of_circle = PI * diameter_of_circle
+print("The circumfrennce of a the circle with the diameter " + str(diameter_of_circle) + " is " + str(circumfrence_of_circle))
